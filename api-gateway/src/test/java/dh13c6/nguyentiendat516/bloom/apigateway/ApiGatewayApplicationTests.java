77305b47757da71b9d0435dd5616633cbcfa048b
ndat1865-spec <ndat1865@gmail.com>
2026-09-23 18:44:06 +0700

    feat(api-gateway): dinh tuyen day du, API Key dong, CORS cho hai frontend

---
+++ api-gateway/src/test/java/dh13c6/nguyentiendat516/bloom/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package dh13c6.nguyentiendat516.bloom.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
